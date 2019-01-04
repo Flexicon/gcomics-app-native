30d50a653238be0f2937570b39fc8cd6c36f674b
Michal Repec <mikerepec@gmail.com>
2019-01-04 13:01:09 +0100

    App setup

---
+++ App.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet, View } from 'react-native'
+import { AppLoading, Asset, Font, Icon } from 'expo'
+import AppNavigator from './navigation/AppNavigator'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+})
+
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  loadResourcesAsync = async () =>
+    Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        ...Icon.Ionicons.font,
+      }),
+    ])
+
+  handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    /* eslint-disable-next-line */
+    console.warn(error)
+  }
+
+  handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true })
+  }
+
+  render() {
+    const { skipLoadingScreen } = this.props
+    const { isLoadingComplete } = this.state
+
+    if (!isLoadingComplete && !skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.loadResourcesAsync}
+          onError={this.handleLoadingError}
+          onFinish={this.handleFinishLoading}
+        />
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <AppNavigator />
+      </View>
+    )
+  }
+}
+
+App.propTypes = {
+  skipLoadingScreen: PropTypes.bool,
+}
+
+App.defaultProps = {
+  skipLoadingScreen: false,
+}
+
+export default App
---
+++ screens/HomeScreen.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+})
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            <Text style={styles.getStartedText}>Hello App!</Text>
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
